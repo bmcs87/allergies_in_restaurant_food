db6b7dc85aa3e93ee75c4aec4574c6796abfdb03
Raghu Betina <raghu@firstdraft.com>
2017-08-15 02:15:13 +0000

    Direct association between restaurants and entrees

--- app/models/entree.rb
+++ app/models/entree.rb
@@ -1,6 +1,9 @@
 class Entree < ApplicationRecord
   # Direct associations
 
+  belongs_to :restaurants,
+             :class_name => "Restaurant"
+
   belongs_to :allergens,
              :class_name => "Allergen"
 
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,11 @@
 class Restaurant < ApplicationRecord
   # Direct associations
 
+  has_many   :cuisines,
+             :class_name => "Entree",
+             :foreign_key => "restaurants_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
