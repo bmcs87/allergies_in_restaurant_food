631eb73695d38a3f9127c13e7db09b72e031f268
Raghu Betina <raghu@firstdraft.com>
2017-08-15 02:15:05 +0000

    Generate users resource with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # Routes for the User resource:
+  # READ
+  get "/users", :controller => "users", :action => "index"
+  get "/users/:id", :controller => "users", :action => "show"
+
+
   # Routes for the Entree resource:
   # CREATE
   get "/entrees/new", :controller => "entrees", :action => "new"
