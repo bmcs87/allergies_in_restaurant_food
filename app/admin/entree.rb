3cf966bbc2b2ee5e1ee2ddfb134a606c5d4c3026
Raghu Betina <raghu@firstdraft.com>
2017-08-15 02:15:51 +0000

    Whitelist columns in Entree for ActiveAdmin

--- app/admin/entree.rb
+++ app/admin/entree.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Entree do
 
+ permit_params :name, :allergens_id, :restaurants_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
