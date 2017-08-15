09220b8becbde8bd2c80db50d10fdbc3b46a4182
Raghu Betina <raghu@firstdraft.com>
2017-08-15 02:15:40 +0000

    Whitelist columns in Allergen for ActiveAdmin

--- app/admin/allergen.rb
+++ app/admin/allergen.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Allergen do
 
+ permit_params :name, :reaction, :restaurants_id, :user_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
