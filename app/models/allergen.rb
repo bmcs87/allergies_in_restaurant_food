9ec2a37477fb18bc2991b24cdbf038a289cb1515
Raghu Betina <raghu@firstdraft.com>
2017-08-15 02:15:13 +0000

    Direct association between allergens and entrees

--- app/models/allergen.rb
+++ app/models/allergen.rb
@@ -1,6 +1,11 @@
 class Allergen < ApplicationRecord
   # Direct associations
 
+  has_many   :cuisines,
+             :class_name => "Entree",
+             :foreign_key => "allergens_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/entree.rb
+++ app/models/entree.rb
@@ -1,6 +1,9 @@
 class Entree < ApplicationRecord
   # Direct associations
 
+  belongs_to :allergens,
+             :class_name => "Allergen"
+
   # Indirect associations
 
   # Validations
